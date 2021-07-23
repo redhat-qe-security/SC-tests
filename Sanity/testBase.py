cf7a4f5902912b493ddaf9ae9bf605efbb6f6751
Pavel Yadlouski <pyadlous@redhat.com>
2021-07-23 16:23:53 +0200

    Update obtaining variables for test from config file

--- Sanity/testBase.py
+++ Sanity/testBase.py
@@ -1,16 +1,13 @@
 # author: Pavel Yadlouski <pyadlous@redhat.com>
-from avocado import Test
 from SCAutolib import log
+from avocado import Test
 
 
 class TestBase(Test):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        log.debug("Preparing varialbes")
-        self.ROOT_PASSWD = self.params.get("root_passwd", path="/run/")
-        self.users = self.params.get("users", path="/run/")
-        log.debug(self.users)
-        self.PIN = "123456"
-        self.PASSWD = "654321"
-        self.USERNAME = "local-user"
-        self.WRONG_PASSWD = "q5435"
+        log.debug("Preparing test variables")
+        self.ROOT_PASSWD = self.params.get("root_passwd", path="/")
+        self.USERNAME_LOCAL = self.params.get("name", path="/local_user/")
+        self.PASSWD_LOCAL = self.params.get("passwd", path="/local_user/")
+        self.PIN_LOCAL = self.params.get("pin", path="/local_user/")
--- Sanity/test_login.py
+++ Sanity/test_login.py
@@ -1,7 +1,7 @@
 # author: Pavel Yadlouski <pyadlous@redhat.com>
 import SCAutolib.src.authselect as authselect
 import SCAutolib.src.virt_card as virt_sc
-
+from SCAutolib import log
 from testBase import TestBase
 
 
@@ -9,49 +9,50 @@ class TestLogin(TestBase):
 
     def test_su_login_with_sc(self):
         """Basic su login to the user with a smart card."""
+
         with authselect.Authselect(required=False):
-            with virt_sc.VirtCard(self.USERNAME, insert=True) as sc:
-                sc.run_cmd(f'su - {self.USERNAME} -c "su - {self.USERNAME} -c whoami"',
-                           expect=self.USERNAME, passwd="123456", pin=True)
+            with virt_sc.VirtCard(self.USERNAME_LOCAL, insert=True) as sc:
+                sc.run_cmd(f'su - {self.USERNAME_LOCAL} -c "su - {self.USERNAME_LOCAL} -c whoami"',
+                           expect=self.USERNAME_LOCAL, passwd=self.PIN_LOCAL, pin=True)
 
     def test_su_login_with_sc_wrong(self):
         """Basic su login to the user with a smart card."""
         with authselect.Authselect(required=False):
-            with virt_sc.VirtCard(self.USERNAME, insert=True) as sc:
-                sc.run_cmd(f'su - {self.USERNAME} -c "su - {self.USERNAME}"',
-                           expect="su: Authentication failure", passwd="9876543", pin=True)
+            with virt_sc.VirtCard(self.USERNAME_LOCAL, insert=True) as sc:
+                sc.run_cmd(f'su - {self.USERNAME_LOCAL} -c "su - {self.USERNAME_LOCAL}"',
+                           expect="su: Authentication failure", passwd="1264325", pin=True)
 
     def test_gdm_login_sc_required(self):
         """GDM login to the user when smart card is enforcing."""
         with authselect.Authselect(required=True):
-            with virt_sc.VirtCard(self.USERNAME) as sc:
-                inner = f'sssctl user-checks -s gdm-smartcard {self.USERNAME} -a auth'
+            with virt_sc.VirtCard(self.USERNAME_LOCAL) as sc:
+                inner = f'sssctl user-checks -s gdm-smartcard {self.USERNAME_LOCAL} -a auth'
                 cmd = f'bash -c "{inner}"'
                 shell = sc.run_cmd(cmd, expect="")
                 shell.expect("Please insert smart card")
                 sc.insert()
-                shell.expect(f"PIN for {self.USERNAME}")
-                shell.sendline(self.PIN)
+                shell.expect(f"PIN for {self.USERNAME_LOCAL}")
+                shell.sendline(self.PIN_LOCAL)
                 shell.expect("pam_authenticate.*Success")
 
     def test_su_login_without_sc(self):
         """SU login without smart card."""
         with authselect.Authselect():
-            with virt_sc.VirtCard(self.USERNAME) as sc:
-                cmd = f'su - {self.USERNAME} -c "su - {self.USERNAME} -c whoami"'
-                sc.run_cmd(cmd, self.USERNAME, pin=False, passwd=self.PASSWD)
+            with virt_sc.VirtCard(self.USERNAME_LOCAL) as sc:
+                cmd = f'su - {self.USERNAME_LOCAL} -c "su - {self.USERNAME_LOCAL} -c whoami"'
+                sc.run_cmd(cmd, self.USERNAME_LOCAL, pin=False, passwd=self.PASSWD_LOCAL)
 
     def test_su_to_root(self):
         """Test for smart card login to the local user and the su - to the root.
         Test is executed under root, this why there is need to do twice login
         into the localuser"""
         with authselect.Authselect(lock_on_removal=True, mk_homedir=True):
-            with virt_sc.VirtCard(self.USERNAME, insert=True) as sc:
-                shell = sc.run_cmd(f'bash -c "su - {self.USERNAME}"', expect="")
-                shell.sendline(f"su - {self.USERNAME}")
-                shell.sendline(self.PIN)
+            with virt_sc.VirtCard(self.USERNAME_LOCAL, insert=True) as sc:
+                shell = sc.run_cmd(f'bash -c "su - {self.USERNAME_LOCAL}"', expect="")
+                shell.sendline(f"su - {self.USERNAME_LOCAL}")
+                shell.sendline(self.PIN_LOCAL)
                 shell.sendline("whoami")
-                shell.expect(self.USERNAME)
+                shell.expect(self.USERNAME_LOCAL)
                 shell.sendline('su - root -c "whoami"')
                 shell.sendline(self.ROOT_PASSWD)
                 shell.expect("root")
