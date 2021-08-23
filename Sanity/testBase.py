8c6fa80954772016117e0037f26753e1b4670565
x00Pavel <pavel.yadlouski@gmail.com>
2021-08-23 10:24:16 +0200

    Replace getting data directly from the yaml file to getting from read_config

--- Sanity/testBase.py
+++ Sanity/testBase.py
@@ -1,5 +1,6 @@
 # author: Pavel Yadlouski <pyadlous@redhat.com>
 from SCAutolib import log
+from SCAutolib.src.env import read_config
 from avocado import Test
 
 
@@ -7,7 +8,7 @@ class TestBase(Test):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         log.debug("Preparing test variables")
-        self.ROOT_PASSWD = self.params.get("root_passwd", path="/")
-        self.USERNAME_LOCAL = self.params.get("name", path="/local_user/")
-        self.PASSWD_LOCAL = self.params.get("passwd", path="/local_user/")
-        self.PIN_LOCAL = self.params.get("pin", path="/local_user/")
+        self.ROOT_PASSWD = read_config("root_password")
+        self.USERNAME_LOCAL = read_config("local_user.name")
+        self.PASSWD_LOCAL = read_config("local_user.passwd")
+        self.PIN_LOCAL = read_config("local_user.pin")
