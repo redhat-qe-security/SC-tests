2d4779d943164f368f45469bc8e9695ea1497a3c
Marek Havrila <mhavrila@redhat.com>
2021-09-14 11:38:37 +0200

    Add test: sssd.conf have matchrule section for wrong user

--- Sanity/test_sssd_conf.py
+++ Sanity/test_sssd_conf.py
@@ -1,5 +1,6 @@
 # author: Pavel Yadlouski <pyadlous@redhat.com>
 import pytest
+import re
 
 from fixtures import *
 from SCAutolib.src.exceptions import PatternNotFound
@@ -23,6 +24,31 @@ def test_su_login_p11_uri_slot_description(user, edit_config):
 def test_su_login_p11_uri_wrong_slot_description(user, edit_config):
     """Test login with password to the system with wrong p11_uri with wrong
     slot description in sssd.conf."""
+    user.su_login_local_with_passwd()
+
+
+@pytest.mark.parametrize("file_path,section,key,value,restore,restart",
+                         [("/etc/sssd/sssd.conf",
+                           f"certmap/shadowutils/{local_user().USERNAME_LOCAL}",
+                           "matchrule", "<SUBJECT>.*CN=testuser.*",
+                           True, ["sssd"])])
+def test_matchrule_defined_for_other_user(user, edit_config):
+    """Test smart card login fail when sssd.conf do not contain
+    [certmap/shadowutils/USER] section for the user from the SC. Instead
+    section for other ([certmap/shadowutils/WRONG_USER]) user is present"""
+    # change section of sssd.conf to get [certmap/shadowutils/testuser]
+    with open("/etc/sssd/sssd.conf", "r") as sources:
+        sourcesdata = sources.read()
+    sourcesdata = sourcesdata.replace(f'[certmap/shadowutils/{user.USERNAME}]', '[certmap/shadowutils/testuser]')
+        with open("/etc/sssd/sssd.conf", "w") as sources:
+            sources.write(sourcesdata)
+    # print sssd.conf; restart service
+    print("Custom changes were made in sssd.conf file:")
+    print(sourcesdata)
+    restart_service("sssd")
+    # run tests
+    with pytest.raises(PatternNotFound):
+        user.su_login_local_with_sc()
     user.su_login_local_with_passwd()
 
 
