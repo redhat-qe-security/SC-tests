398683c0a5755bc3242bf5fb3e0696660d0ede81
x00Pavel <pavel.yadlouski@gmail.com>
2021-10-29 10:10:51 +0200

    Merge tests for changing the kerberos password

--- Kerberos/test_kerberos_user_change_password.py
+++ Kerberos/test_kerberos_user_change_password.py
@@ -1,123 +1,72 @@
+import pytest
 from SCAutolib.src.authselect import Authselect
 from SCAutolib.src.virt_card import VirtCard
-from fixtures import ipa_user_indirect, user_shell
+
+from fixtures import ipa_user_, ipa_user_indirect, user_shell
 
 
-def test_kerberos_change_passwd_sc_login(ipa_user, user_shell):
-    """Kerberos user tries to change it kerberos password after user is logged
-    in to the system with smartcard when smartcard is not required for login.
+@pytest.mark.parametrize("required,insert,expect,secret",
+                         [(False, False, "Password:", ipa_user_().PASSWD),
+                          (False, True, f"PIN for {ipa_user_().USERNAME}:",
+                           ipa_user_().PIN),
+                          (True, False, "Password:", ipa_user_().PASSWD),
+                          (True, True, f"PIN for {ipa_user_().USERNAME}:",
+                           ipa_user_().PIN)])
+def test_kerberos_change_passwd(ipa_user, user_shell, required, insert, expect,
+                                secret):
+    """Kerberos user tries to change it kerberos password after he is logged
+    in to the system.
 
-    Setup
-        1. General setup
-        2. Setup authselect: authselect select sssd with-smartcard
-        3. Switch to kerberos user (su kerberos_username)
-        4. Insert the card
-        5. Login with smartcard PIN
-        6. Try to change the password by 'passwd'
+    This test runs 4 times with different set of parameters. Sets are:
+        - smart card is not required, smart card is not inserted, expecting 'Password:'
+          on the stdout, passing correct user password.
+        - smart card is not required, smart card is inserted, expecting
+          'PIN for user_name:', passing correct smart card PIN.
 
-    Expected result
-        - Message about changing the password is written to the console
-     """
-    with Authselect():
-        with VirtCard(ipa_user.USERNAME, insert=True) as f:
-            cmd = f"su {ipa_user.USERNAME} -c 'passwd'"
-            user_shell.sendline(cmd)
-            user_shell.expect_exact(f"PIN for {ipa_user.USERNAME}:")
-            user_shell.sendline(ipa_user.PIN)
-            user_shell.expect_exact(f"Changing password for user {ipa_user.USERNAME}.")
+        - smart card is required, smart card is not inserted, expecting 'Password:'
+          on the stdout, passing correct user password.
+        - smart card is required, smart card is inserted, expecting
+          'PIN for user_name:', passing correct smart card PIN.
 
+    In this way we test 4 scenarios in one test:
+        - testing that user can login with the password and change the password
+        - testing that user can login with the smart card and change the
+          password
+        - testing that smart card is not enforced for 'su' command by setting
+          with-smartcard-required parameter to authselect, so user can still
+          login with a password and change his password without using the smart card.
+        - testing that smart card is not mentioned when user change password
+          after he logged in with smart card and smart card is required for the
+          login (with-smartcard-required parameter to authselect)
 
-def test_kerberos_change_passwd_sc_login_required(ipa_user, user_shell):
-    """Kerberos user tries to change it kerberos password after user is logged
-    in to the system with smartcard when smartcard is required for login.
-
-    Setup
-        1. General setup
-        2. Setup authselect: authselect select sssd with-smartcard with-smartcard-required
-        3. Switch to kerberos user (su kerberos_username)
-        4. Insert the card
-        5. Login to the system with smartcard PIN
-        6. Try to change the password by 'passwd'
-
-    Expected result
-        - Message about changing the password is written to the console
-     """
-    with Authselect():
-        with VirtCard(ipa_user.USERNAME, insert=True) as f:
-            cmd = f"su {ipa_user.USERNAME} -c 'passwd'"
-            user_shell.sendline(cmd)
-            user_shell.expect_exact(f"PIN for {ipa_user.USERNAME}:")
-            user_shell.sendline(ipa_user.PIN)
-            user_shell.expect_exact(f"Changing password for user {ipa_user.USERNAME}.")
-
-
-def test_kerberos_change_passwd_password_login_required(ipa_user, user_shell):
-    """Kerberos user tries to change it kerberos password after user is logged
-    in to the system with password when smartcard is required for login.
-
+    As expected behaviour form user site looks similar, scenarios with same
+    insert, expect and secret parts are using similar description
     Setup
         1. General setup
         2. Setup authselect: authselect select sssd with-smartcard with-smartcard-required
-        3. Login to kerberos user with smart card (su kerberos_username)
-        4. Insert smartcard
-        5. Put correct smart card pin
-        6. Try to change the password by 'passwd'
 
-    Expected result
-        - Message about changing the password is written to the console
+    Scenario 1, 3:
+        1. Do not insert the smart card
+        2. Try to switch the user to kerberos user (su kerberos_user -c 'passwd')
+           from the normal user
+    Expected result:
+        - User is asked for password
+        - After correct password is inserted, user is successfully logged in
+        - Warning about changing the password is shown
+
+    Scenario 2, 4:
+        1. User insert the card
+        2. Try to switch the user to kerberos user (su kerberos_user -c 'passwd')
+           from the normal user
+    Expected result:
+        - User is asked for smart card PIN
+        - After correct PIN is inserted, user is successfully logged in
+        - Warning about changing the password is shown
      """
-    with Authselect(required=True):
-        with VirtCard(ipa_user.USERNAME, insert=False) as sc:
+    with Authselect(required=required):
+        with VirtCard(ipa_user.USERNAME, insert=insert) as f:
             cmd = f"su {ipa_user.USERNAME} -c 'passwd'"
             user_shell.sendline(cmd)
-            user_shell.expect_exact(f"Please insert smart card")
-            sc.insert()
-            user_shell.expect_exact(f"PIN for {ipa_user.USERNAME}:")
-            user_shell.sendline(ipa_user.PIN)
-            user_shell.expect_exact(f"Changing password for user {ipa_user.USERNAME}.")
-
-
-def test_kerberos_change_passwd_password_login(ipa_user, user_shell):
-    """Kerberos user tries to change it kerberos password after user is logged
-    in to the system with password when smartcard is not required for login.
-
-    Setup
-        1. General setup
-        2. Setup authselect: authselect select sssd with-smartcard
-        3. Login to kerberos user with password (su kerberos_username)
-        4. Try to change the password by 'passwd'
-
-    Expected result
-        - Message about changing the password is written to the console
-     """
-    with Authselect():
-            cmd = f"su {ipa_user.USERNAME} -c 'passwd'"
-            user_shell.sendline(cmd)
-            user_shell.expect_exact(f"Password:")
-            user_shell.sendline(ipa_user.PASSWD)
-            user_shell.expect_exact(f"Changing password for user {ipa_user.USERNAME}.")
-
-
-# @pytest.mark.skip(reason="Need fix due to specific password changing with IPA server")
-def test_kerberos_change_passwd(ipa_user, user_shell):
-    """Kerberos user tries to change it kerberos password after user is logged
-    in to the system with smartcard
-
-    Setup
-        1. General setup
-        2. Setup authselect: authselect select sssd with-smartcard
-        3. Switch to kerberos user (su kerberos_username)
-        4. Insert the card
-        5. Try to change the password by 'passwd'
-
-    Expected result
-        - Message about changing the password is written to the console
-     """
-    # FIXME: test fails due to IPA password policies, so it is not stable
-    with Authselect():
-        with VirtCard(ipa_user.USERNAME, insert=True) as f:
-            cmd = f"su {ipa_user.USERNAME} -c 'passwd'"
-            user_shell.sendline(cmd)
-            user_shell.expect_exact(f"PIN for {ipa_user.USERNAME}:")
-            user_shell.sendline(ipa_user.PIN)
+            user_shell.expect_exact(expect)
+            user_shell.sendline(secret)
             user_shell.expect_exact(f"Changing password for user {ipa_user.USERNAME}.")
