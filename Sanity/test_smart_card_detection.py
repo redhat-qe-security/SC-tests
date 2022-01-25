6b347bf4abbfb91e12ee63e870d1d493b27fc15f
x00Pavel <pavel.yadlouski@gmail.com>
2022-01-25 17:48:05 +0100

    Add docstring

--- Sanity/test_smart_card_detection.py
+++ Sanity/test_smart_card_detection.py
@@ -3,6 +3,8 @@ from SCAutolib.src import virt_card
 
 
 def test_modutil_token_info(user, root_shell):
+    """Check that p11-kit module shows smart card information with modutil
+    command"""
     cmd = "modutil -list -dbdir /etc/pki/nssdb"
     uri = f"pkcs11:token={user.USERNAME_LOCAL};manufacturer=Common%20Access" \
           f"%20Card;serial=000058bd002c19b5;model=PKCS%2315%20emulated"
