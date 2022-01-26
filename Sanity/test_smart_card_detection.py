bd33a5cd7a2057c905014308002b8a2a0637b52b
x00Pavel <pavel.yadlouski@gmail.com>
2022-01-26 12:05:14 +0100

    Add test case for configurable PAM module with sc auth

--- Sanity/test_smart_card_detection.py
+++ Sanity/test_smart_card_detection.py
@@ -1,5 +1,7 @@
-from fixtures import user_indirect, root_shell
-from SCAutolib.src import virt_card
+import pytest
+
+from fixtures import user_indirect, root_shell, edit_config
+from SCAutolib.src import virt_card, utils
 
 
 def test_modutil_token_info(user, root_shell):
@@ -11,3 +13,31 @@ def test_modutil_token_info(user, root_shell):
     with virt_card.VirtCard(user.USERNAME_LOCAL, insert=True):
         root_shell.sendline(cmd)
         root_shell.expect_exact(uri)
+
+
+@pytest.mark.parametrize("file_path,section,key,value,restore,restart",
+                         [("/etc/sssd/sssd.conf", "pam",
+                           "pam_p11_allowed_services", "-su", True, ["sssd"])])
+def test_pam_services_config(user, root_shell, edit_config):
+    """Test for PAM configuration for smart card authentication.
+    GitHub issue: https://github.com/SSSD/sssd/issues/3967"""
+
+    with virt_card.VirtCard(user.USERNAME_LOCAL, insert=True):
+        cmd = "sssctl user-checks -a auth -s pam_cert_service " \
+              f"{user.USERNAME_LOCAL}"
+        root_shell.sendline(cmd)
+        fail = f"pam_authenticate for user [{user.USERNAME_LOCAL}]: " \
+               "Authentication service cannot retrieve authentication info"
+        root_shell.expect_exact("Please insert smart card")
+        root_shell.expect_exact("Password:")
+        root_shell.sendline(user.PASSWD_LOCAL)
+        root_shell.expect_exact(fail)
+
+        utils.edit_config_("/etc/sssd/sssd.conf", "pam",
+                           "pam_p11_allowed_services", "+pam_cert_service")
+        utils.restart_service("sssd")
+
+        root_shell.sendline(cmd)
+        root_shell.expect_exact(f"PIN for {user.USERNAME_LOCAL}:")
+        root_shell.sendline(user.PIN_LOCAL)
+        root_shell.expect_exact(f"pam_authenticate for user [{user.USERNAME_LOCAL}]: Success")
