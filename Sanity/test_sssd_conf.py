0e3c579c6b5a0d4978b6af1ef04ac925dd99e47f
Pavel Yadlouski <pyadlous@redhat.com>
2022-09-23 15:16:48 +0200

    Adopt sssd tests

--- Sanity/test_sssd_conf.py
+++ Sanity/test_sssd_conf.py
@@ -1,105 +1,78 @@
 # author: Pavel Yadlouski <pyadlous@redhat.com>
-from fixtures import *
-from SCAutolib.src.exceptions import PatternNotFound
-from SCAutolib.src.utils import (edit_config_, backup_)
-from SCAutolib.src import env_logger
+
 import pytest
 
+from SCAutolib.models.authselect import Authselect
+from SCAutolib.utils import run
+from conftest import log, local_user as local_user_conftest
 
-@pytest.mark.skip
-@pytest.mark.parametrize("file_path,target,restore,restart",
-                         [("/etc/sssd/sssd.conf",
-                          {"section": "pam",
-                            "key": "p11_uri",
-                            "val": "pkcs11:slot-description=Virtual%20PCD%2000%2000"},
-                           True, ["sssd"])])
-def test_su_login_p11_uri_slot_description(user, edit_config):
+
+@pytest.mark.parametrize("uri", ["pkcs11:slot-id=0", "wrong-uri"])
+def test_su_login_p11_uri_slot_description(local_user, uri, sssd, user_shell):
     """Test login with PIN to the system with p11_uri specified on specific
-    slot in sssd.conf."""
-    user.su_login_local_with_sc()
+    slot in sssd.conf.
+
+    Expected result:
+        - when correct uri is set, user is prompted to insert the PIN
+        - when incorrect uri is set, user is not prompted to insert the password
+    """
+    with sssd(section="pam", key="p11_uri",
+              value=uri):
+        run(["ls", "-l", "/etc/sssd/sssd.conf"])
+        with open("/etc/sssd/sssd.conf", "r") as f:
+            print(f.read())
+
+        with Authselect(required=False), local_user.card(insert=True):
+            cmd = f"su {local_user.username} -c whoami"
+            user_shell.sendline(cmd)
+            if uri == "wrong-uri":
+                user_shell.expect_exact("Password:")
+                user_shell.sendline(local_user.password)
+            else:
+                user_shell.expect(f"PIN for {local_user.username}")
+                user_shell.sendline(local_user.pin)
+            index = user_shell.expect([local_user.username, "su: Authentication failure"])
+            if index == 1:
+                log.error("User was not able to login")
+                pytest.fail("Login is not sucessfull")
 
 
-@pytest.mark.parametrize("file_path,target,restore,restart",
-                         [("/etc/sssd/sssd.conf",
-                           {"section": "pam",
-                             "key": "p11_uri",
-                             "val": "pkcs11:slot-description=Virtual%20PCD%2000%2001"},
-                           True, ["sssd"])])
-def test_su_login_p11_uri_wrong_slot_description(user, edit_config):
-    """Test login with password to the system with wrong p11_uri with wrong
-    slot description in sssd.conf."""
-    user.su_login_local_with_passwd()
-
-
-@pytest.mark.parametrize("file_path,target,restore,restart",
-                         [("/etc/sssd/sssd.conf",
-                          {"section": f"certmap/shadowutils/{local_user().USERNAME_LOCAL}",
-                            "key": "matchrule",
-                            "val": "<SUBJECT>.*CN=testuser.*"},
-                           True, ["sssd"])])
-def test_matchrule_defined_for_other_user(user, edit_config):
+def test_matchrule_defined_for_other_user(local_user, sssd, user_shell):
     """Test smart card login fail when sssd.conf do not contain
     [certmap/shadowutils/USER] section for the user from the SC. Instead,
     section for other ([certmap/shadowutils/WRONG_USER]) user is present"""
     # change section of sssd.conf to get [certmap/shadowutils/testuser]
-    with open("/etc/sssd/sssd.conf", "r") as sources:
-        sourcesdata = sources.read()
-    sourcesdata = sourcesdata.replace(
-        f'[certmap/shadowutils/{user.USERNAME_LOCAL}]',
-        '[certmap/shadowutils/testuser]')
-    with open("/etc/sssd/sssd.conf", "w") as sources:
-        sources.write(sourcesdata)
-    # print sssd.conf; restart service
-    env_logger.warning("Custom changes were made in sssd.conf file:")
-    env_logger.info(sourcesdata)
-    restart_service("sssd")
-    # run tests
-    with pytest.raises(PatternNotFound):
-        user.su_login_local_with_sc()
-    user.su_login_local_with_passwd()
+    with sssd(section=f"certmap/shadowutils/{local_user.username}",
+              key="matchrule",
+              value="<SUBJECT>.*CN=testuser.*") as sssd_file:
+        # FIXME: this section should be replaced with library call for removing
+        #  the section as sson as this functionality is implemented
+        with sssd_file.path.open("r+") as sources:
+            sourcesdata = sources.read()
+            sourcesdata = sourcesdata.replace(
+                f'[certmap/shadowutils/{local_user.username}]',
+                '[certmap/shadowutils/testuser]')
+            sources.write(sourcesdata)
+        run(["systemctl", "restart", "sssd"])
+
+        with Authselect(required=False), local_user.card(insert=True):
+            cmd = f"su {local_user.username} -c whoami"
+            user_shell.sendline(cmd)
+            user_shell.expect_exact("Password:")
+            user_shell.sendline(local_user.password)
+            user_shell.expect(local_user.username)
 
 
-@pytest.mark.parametrize("file_path,target,restore,restart",
-                         [("/etc/sssd/sssd.conf",
-                           {"section": "pam",
-                             "key": "p11_uri",
-                             "val": "pkcs11:slot-description=Virtual%20PCD%2000%2000"},
-                           True, ["sssd"])])
-def test_su_login_p11_uri_user_mismatch(user, edit_config):
+@pytest.mark.parametrize("rule", ["<SUBJECT>.*CN=testuser.*", f"<SUBJECT>.*UID={local_user_conftest.username}.*"])
+def test_user_mismatch(local_user, sssd, user_shell, rule):
     """Test smart card login fail when sssd.conf do not contain user from
     the smart card (wrong user in matchrule)"""
-    edit_config_("/etc/sssd/sssd.conf",
-                 f"certmap/shadowutils/{user.USERNAME_LOCAL}",
-                 "matchrule",
-                 "<SUBJECT>.*CN=testuser.*")
-    destination_path = backup_("/etc/sssd/sssd.conf")
-    restart_service("sssd")
-    with pytest.raises(PatternNotFound):
-        user.su_login_local_with_sc()
-    user.su_login_local_with_passwd()
-
-
-@pytest.mark.parametrize("file_path,target,restore,restart",
-                         [("/etc/sssd/sssd.conf",
-                           {"section": f"certmap/shadowutils/{local_user().USERNAME_LOCAL}",
-                             "key": "matchrule",
-                             "val": "<SUBJECT>.*CN=testuser.*"},
-                           True, ["sssd"])])
-def test_user_mismatch(user, edit_config):
-    """Test smart card login fail when sssd.conf do not contain user from
-    the smart card (wrong user in matchrule)"""
-    with pytest.raises(PatternNotFound):
-        user.su_login_local_with_sc()
-    user.su_login_local_with_passwd()
-
-
-@pytest.mark.parametrize("file_path,target,restore,restart",
-                         [("/etc/sssd/sssd.conf",
-                           {"section": f"certmap/shadowutils/{local_user().USERNAME_LOCAL}",
-                            "key": "matchrule",
-                            "val": f"UID={local_user().USERNAME_LOCAL}"},
-                           True, ["sssd"])])
-def test_wrong_subject_in_matchrule(user, edit_config):
-    """Test smart card login fail when sssd.conf contain wrong subject in
-    the matchrule."""
-    user.su_login_local_with_passwd()
+    with sssd(section=f"certmap/shadowutils/{local_user.username}",
+              key="matchrule",
+              value=rule):
+        with Authselect(required=False), local_user.card(insert=True):
+            cmd = f"su {local_user.username} -c whoami"
+            user_shell.sendline(cmd)
+            user_shell.expect_exact("Password:")
+            user_shell.sendline(local_user.password)
+            user_shell.expect(local_user.username)
