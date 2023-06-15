24da6b6012411cedad616c2b2c606999d6d4c735
George Pantelakis <gpantela@redhat.com>
2023-06-15 10:23:42 +0200

    Fixing test_krb_user_sudo_correct_password_sc_required_no_sc
    This test was never working because the allowed sudo commands
    needed to be added to IPA server. With this commit we fix this
    issue by creating a new fixture that allows all commands with
    sudo for the tested user.

--- fixtures.py
+++ fixtures.py
@@ -1,7 +1,9 @@
 import pexpect
 import pytest
 import sys
+import logging
 
+from SCAutolib import run
 from SCAutolib.models.file import SSSDConf
 from SCAutolib.utils import user_factory
 
@@ -21,6 +23,24 @@ def root_shell():
     shell.logfile = sys.stdout
     return shell
 
+@pytest.fixture(scope="function")
+def allow_sudo_commands(ipa_user):
+    """
+    Modifying the IPA server's sudo rules to allow the test user to 
+    run sudo commands and restore the original state afterward.
+    """
+    logger = logging.getLogger()
+
+    run('ipa sudorule-add allow_sudo --hostcat=all --runasusercat=all --runasgroupcat=all --cmdcat=all'.split())
+    run(f'ipa sudorule-add-user allow_sudo --user {ipa_user.username}'.split())
+    run("systemctl restart sssd".split(), sleep = 5)
+    logger.debug("Checking that the sudo rule has been added (following command should succeed)")
+    run('ipa sudorule-show allow_sudo'.split())
+    yield # running the test's code
+    run('ipa sudorule-del allow_sudo'.split())
+    run("systemctl restart sssd".split(), sleep = 5)
+    logger.debug("Checking that the sudo rule has been removed (following command should exit with status 2)")
+    run('ipa sudorule-show allow_sudo'.split(), return_code = [2])
 
 @pytest.fixture(scope="session")
 def root_user():
